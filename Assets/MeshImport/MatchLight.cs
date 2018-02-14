eec6097d23cff2d4184d2d4f81a17981aa87bcb4
Richard Leinfellner <richard@modulo17.com>
2018-02-14 00:28:59 +0000

    Added Character pack & 3D test scene

---
+++ Assets/MeshImport/MatchLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchLight : MonoBehaviour {
+
+    MeshRenderer mMR;
+
+    [SerializeField]
+    Light ColourLight;
+
+    [SerializeField]
+    Color MatColour;
+
+	// Use this for initialization
+	void Start () {
+        mMR = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mMR.material.color = ColourLight.color;
+	}
+}
